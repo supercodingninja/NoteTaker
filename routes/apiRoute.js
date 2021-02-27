b34d7928a2d369d804787ab99b0b094725ed9cfc
SCN <supercodingninja@outlook.com>
2021-02-27 07:01:40 -0800

    Created variable dependacies; and pseudo-code (I need to rest).

---
+++ routes/apiRoute.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+// Criterea requires db.json file: so the application is going to store & retrieve notes from the `fs` module (the `db.json` file on the backend). //
+
+// C. R. U. D. //
