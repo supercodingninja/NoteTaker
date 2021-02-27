bafc1c1b8d95511155905e5bfb6283f28dabbc1a
SCN <supercodingninja@outlook.com>
2021-02-27 07:06:15 -0800

    CORRECTION ON THOUGHT: More pseudo-code to help me stay on track; and follow the criterea.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -5,3 +5,6 @@ const fs = require('fs');
 // Criterea requires db.json file: so the application is going to store & retrieve notes from the `fs` module (the `db.json` file on the backend). //
 
 // C. R. U. D. //
+// POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. //
+
+// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. //
---
+++ routes/htmlRoute.js
@@ -0,0 +1,7 @@
+// C. R. U. D. //
+// GET `/notes` - Should return the `notes.html` file. //
+
+
+// GET `*` - Should return the `index.html` file. //
+
+// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON. //
