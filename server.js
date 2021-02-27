0900106db7aba6086a342fec45655f428c7cc82c
SCN <supercodingninja@outlook.com>
2021-02-27 06:06:20 -0800

    Required Dependacies, and Setup for the Express Application.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // Required Dependacies. //
+const fs = require('fs');
+const express = require('express');
 
+// YOU NEED THIS TO SETUP THE EXPRESS APPLICATION. //
+const app = express();
 
 // PORT Setup. //
+const PORT = process.env.PORT || 8080;
 
 
 // Need some kind of api route. //
