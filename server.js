2f4e1eeb8f740cb0be9e57be10431c1f87669c0d
SCN <supercodingninja@outlook.com>
2021-02-27 06:22:53 -0800

    Data parsing.

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 
+// YOU NEED A WAY TO PARSE THE DATA. //
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 // Need some kind of api route. //
 require('./routes/apiRoute.js')(app);
 require('./routes/htmlRoute')(app);
