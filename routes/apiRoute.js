8a022215c2fa9711a307cd9a433a62d721fffdf4
SCN <supercodingninja@outlook.com>
2021-02-28 21:06:34 -0800

    Installed packages,  msde declarations; and started functions.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,7 +4,16 @@ const fs = require('fs');
 
 // Criterea requires db.json file: so the application is going to store & retrieve notes from the `fs` module (the `db.json` file on the backend). //
 
-// C. R. U. D. //
+
+module.exports = function(app) {
+
+    // Get //
+}
+
+
+
+// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON. //
+
 // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. //
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. //
--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,7 +1,21 @@
+const path = require('path');
+
 // C. R. U. D. //
-// GET `/notes` - Should return the `notes.html` file. //
+module.exports = function(app) {
+
+    // GET `/notes` - Should return the `notes.html` file. //
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+      });
+    
+      // GET `*` - Should return the `index.html` file. //
+      app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+      });
+}
+
+
+
 
 
 // GET `*` - Should return the `index.html` file. //
-
-// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON. //
