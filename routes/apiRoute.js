97ef5b90402f4a2ce7e9c6ae16de203dd12c75fa
SCN <supercodingninja@outlook.com>
2021-03-03 23:42:49 -0800

    Correction: I should have made 'var JSONdata = require('../db/db.json')' into a global declaration 'const JSONdata = require('../db/db.json')'

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const deleteID = req.params.id;
+const JSONdata = require('../db/db.json')
 
 
 module.exports = function(app) {
@@ -16,8 +16,6 @@ module.exports = function(app) {
 
   // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. //
   app.post('/api/notes', function(req, res) {
-    
-    var JSONdata = require('../db/db.json')
 
     var getNote = req.body;
 
@@ -37,6 +35,8 @@ module.exports = function(app) {
 
   // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. //
   app.delete('/api/notes/:id', function(req, res) {
+    
+    var deleteID = req.params.id;
 
     for (var i = 0; i , JSONdata.length; i++) {
 
