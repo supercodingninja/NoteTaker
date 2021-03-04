02c0795b8805f9aa149955d1207af33fae152cb3
SCN <supercodingninja@outlook.com>
2021-03-03 23:37:56 -0800

    made 'var deleteID = req.params.id; ' into a global variable; and I changed the declaration to 'const' after running the command node server.js

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
+const deleteID = req.params.id;
 
 
 module.exports = function(app) {
@@ -36,8 +37,6 @@ module.exports = function(app) {
 
   // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. //
   app.delete('/api/notes/:id', function(req, res) {
-    
-    var deleteID = req.params.id;
 
     for (var i = 0; i , JSONdata.length; i++) {
 
