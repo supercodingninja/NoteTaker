f47e67a74b142fc46155f01dbf57ad6007722172
SCN <supercodingninja@outlook.com>
2021-03-03 23:12:11 -0800

    Completed Post /api/notes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-// Criterea requires db.json file: so the application is going to store & retrieve notes from the `fs` module (the `db.json` file on the backend). //
-
 
 module.exports = function(app) {
 
@@ -16,5 +14,20 @@ module.exports = function(app) {
 
 
 // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. //
+app.post('/api/notes', function(req, res) {
+  
+  var JSONdata = require('../db/db.json')
+
+  var getNote = req.body;
+
+  JSONdata.push(getNote);
+
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(JSONdata, null), (err) => {
+      if (err) throw err;
+    })
+
+    res.JSON(getNote);
+});
+
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. //
